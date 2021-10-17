57415a13f7badbeff6f115f64468ed3e756eae5d
Aleksandr <a-meretskiy@mail.ru>
2021-10-17 16:41:09 +0300

    hw01 (add background) (#1)
    * hw01 (add background)
    
    * hw01 (add background)

--- core/src/com/meretskiy/game/MyStarGame.java
+++ core/src/com/meretskiy/game/MyStarGame.java
@@ -10,6 +10,7 @@ import com.badlogic.gdx.utils.ScreenUtils;
 public class MyStarGame extends ApplicationAdapter {
 	SpriteBatch batch; //батчер - отвечает за отрисовку объектов, формирует список на отрисовку и передает его в графический процессор
 	Texture img; //текстура - объект хранит вс себе текстуру которую вытаскиваем из папки assets по названию
+	Texture background;
 	TextureRegion region; //позволяет вырезать кусок текстуры по задданным параметрам
 	int x;
 	int y;
@@ -20,6 +21,7 @@ public class MyStarGame extends ApplicationAdapter {
 	public void create () { //инициализация объектов
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		background = new Texture("сosmos.jpeg");
 		region = new TextureRegion(img, 25,25,100,100); //не хранит текстуру, а только ссылку на нее
 	}
 
@@ -27,6 +29,7 @@ public class MyStarGame extends ApplicationAdapter {
 	public void render () { //отрисовка и вся логика, срабатывает 60 раз в секунду
 		ScreenUtils.clear(Color.BLACK); //очистка экрана или в RGB или через Color.CLEAR
 		batch.begin(); // начало списка на отрисовку
+		batch.draw(background, 0, 0);
 		batch.setColor(1,1,1,0.5f); //устанавливаем цвет и прозрачность для последующих отрисовок, если указать еще одну, то она применится к последующим
 		batch.draw(img, x, y, width, height); //рисуем
 		batch.draw(region, 300, 300); //какая отрисовка последняя такая картинка сверху и будет
@@ -39,5 +42,6 @@ public class MyStarGame extends ApplicationAdapter {
 	public void dispose () { //выгрузка объектов из ппамяти по завершению работы
 		batch.dispose();
 		img.dispose();
+		background.dispose();
 	}
 }
