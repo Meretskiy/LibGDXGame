f782213af0489a2c41eaaba4e3f882a5692a5b96
Meretskiy Aleksandr <a-meretskiy@mail.ru>
2021-10-18 00:04:04 +0300

    hw2 - add movement

--- core/src/com/meretskiy/game/screen/MenuScreen.java
+++ core/src/com/meretskiy/game/screen/MenuScreen.java
@@ -11,36 +11,45 @@ import com.meretskiy.game.base.BaseScreen;
 public class MenuScreen extends BaseScreen {
 
     private Texture img;
+    private Texture backGround;
     private Vector2 touch;
+    private Vector2 pos;
     private Vector2 v;
+    private Vector2 temp;
 
     @Override
     public void show() {
         super.show();
-        img = new Texture("badlogic.jpg");
+        img = new Texture("rocket.png");
+        backGround = new Texture("sci-fi-space.jpeg");
         touch = new Vector2();
-        v = new Vector2(1, 1);
-
+        pos = new Vector2();
+        v = new Vector2();
+        temp = new Vector2();
     }
 
     @Override
     public void render(float delta) {
         super.render(delta);
+        temp.set(touch);
+        pos = temp.sub(pos).len() > 1 ? pos.add(v) : pos.set(touch);
         batch.begin();
-        batch.draw(img, touch.x, touch.y);
+        batch.draw(backGround, 0, 0);
+        batch.draw(img, pos.x, pos.y, 150, 200);
         batch.end();
-        touch.add(v);
     }
 
     @Override
     public void dispose() {
         super.dispose();
         img.dispose();
+        backGround.dispose();
     }
 
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
         touch.set(screenX, Gdx.graphics.getHeight() - screenY);
+        v.set(touch.cpy().sub(pos)).nor();
         return super.touchDown(screenX, screenY, pointer, button);
     }
 }
